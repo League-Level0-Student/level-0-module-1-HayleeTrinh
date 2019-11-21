974e28fd771e92c9f3878d5d04216e483ce97112
CalebVia <55600946+CalebVia@users.noreply.github.com>
2019-11-21 15:57:47 -0800

    (Unbirthday WIP. Skipped for now.) (TheRiddler is finished) (Work on my ages)

---
+++ src/_02_unbirthday/Unbirthday.java
@@ -0,0 +1,12 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Hello! Just wondering... When's your birthday?");
+	String bday = JOptionPane.showInputDialog(null, "For example, mine's on 07/10. July tenth.");
+	var today = new Date();
+	
+}
+}
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,16 +12,42 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+int score = 0;
+		//riddle 1
+String candle = JOptionPane.showInputDialog(null, "When i'm young i'm tall. And when i'm old i'm short. What am I?");
+		
+if (candle.equalsIgnoreCase("candle")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score++;
+}
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+else {
+	JOptionPane.showMessageDialog(null, "Wrong. The correct answe is a candle.");
+}
+//riddle 2
+String stamp = JOptionPane.showInputDialog(null, "What can travel around the world while staying in a corner?");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+if (stamp.equalsIgnoreCase("stamp")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score++;
+}
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+else {
+	JOptionPane.showMessageDialog(null, "Wrong. The correct answe is a stamp.");
+}
+//riddle 3
+String clock = JOptionPane.showInputDialog(null, "What has hands but can not clap?");
 
-		// 6. Add some more riddles
+if (clock.equalsIgnoreCase("clock")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score++;
+}
 
-		// 2. Make a pop up to show the score.
+else {
+	JOptionPane.showMessageDialog(null, "Wrong. The correct answe is a clock.");
+}
+//score
+		JOptionPane.showMessageDialog(null, "Nice! Your final score is " + score + "!");
 		
 	}
 }
