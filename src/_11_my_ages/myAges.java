6cd25cb06ac68679891d39417048d4ee84d19170
CalebVia <55600946+CalebVia@users.noreply.github.com>
2019-12-05 15:55:37 -0800

    MyAges completed SecrectMessageBox completed

--- src/_11_my_ages/myAges.java
+++ src/_11_my_ages/myAges.java
@@ -6,8 +6,8 @@ public class myAges {
 public static void main(String[] args) {
 	int age = 0;
 	for (int i = 0; i < 13; i++) {
-		JOptionPane.showMessageDialog(null, "age++");
-		
+		JOptionPane.showMessageDialog(null, age + " years old");
+		age = age+1;
 	}
 }
 }
